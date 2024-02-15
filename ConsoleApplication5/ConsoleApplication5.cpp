03221b43f2ae1735682d519ecc0ec2d7e2820887
Pokrishk <anna.kononova41@gmail.com>
2024-02-15 12:32:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    double arr[3][5];
+    for (int i = 0; i < 3; ++i) {
+        cout << "Введите элементы строки " << i + 1 << ": ";
+        for (int j = 0; j < 5; ++j) {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < 3; ++i) {
+        double sum = 0;
+        for (int j = 0; j < 5; ++j) {
+            sum += arr[i][j];
+        }
+        double average = sum / 5;
+        cout << "Среднее арифметическое элементов строки " << i + 1 << ": " << average << endl;
+    }
+}
